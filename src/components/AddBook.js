34d907be09f1b4c0cc381a3e47475d7290c7f852
ekalbers <ekalbers22@gmail.com>
2023-03-01 14:29:26 -0600

    added modal for create book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 class AddBook extends React.Component {
     submitBook = (event) => {
@@ -11,30 +12,34 @@ class AddBook extends React.Component {
             description: event.target.formDescription.value,
             status: event.target.formHaveRead.checked
         }
-
+        this.props.toggle();
         this.props.postBook(data);
     }
 
     render() {
         return (
-            <Form onSubmit={this.submitBook}>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Title: </Form.Label>
-                    <Form.Control type="name" placeholder="Enter Book Title" id="formTitle" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Author: </Form.Label>
-                    <Form.Control type="name" placeholder="Enter Book Author" id="formAuthor" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Description: </Form.Label>
-                    <Form.Control type="name" placeholder="Enter Book Description" id="formDescription" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Check type="checkbox" label="Have Read" id="formHaveRead" />
-                </Form.Group>
-                <Button variant="primary" type="submit" >Add Book</Button>
-            </Form>
+            <Modal show={this.props.show} onHide={this.props.toggle} centered>
+                <Modal.Body>
+                    <Form onSubmit={this.submitBook}>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Title: </Form.Label>
+                            <Form.Control type="name" placeholder="Enter Book Title" id="formTitle" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Author: </Form.Label>
+                            <Form.Control type="name" placeholder="Enter Book Author" id="formAuthor" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Description: </Form.Label>
+                            <Form.Control type="name" placeholder="Enter Book Description" id="formDescription" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" >
+                            <Form.Check type="checkbox" label="Have Read" id="formHaveRead" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" >Add Book</Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
         );
     }
 }
